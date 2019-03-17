22fd94b58cc51e85a227a88b8adebbae8ecf7606
m-maslennikov <m.maslennikov@outlook.com>
2019-03-18 11:39:25 +1300

    cleanup

--- contact.php
+++ contact.php
@@ -79,11 +79,6 @@
   </div>
   <!-- /.container -->
 
-  <!-- Contact form JavaScript -->
-  <!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-  <script src="js/jqBootstrapValidation.js"></script>
-  <script src="js/contact_me.js"></script>
-  
   <!-- Google Map-->
   <script src="js/googleMap.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2vEBgLA6uUDcbWDj01hEh4r52cVBg3-M&amp;callback=myMap"></script>
