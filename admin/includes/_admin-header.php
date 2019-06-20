61c91505f6bc6c870f08f209c2848dbb61d922ae
cmralvorada <cassiomagalhaesdosreis@gmail.com>
2019-06-20 23:03:08 +1200

    Admin Dropdown Items

--- admin/includes/_admin-header.php
+++ admin/includes/_admin-header.php
@@ -307,20 +307,20 @@ if(!isAdmin() && !isStaff()) {redirect("../");} ?>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="profile.php">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
+                  My Profile
                 </a>
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="history.php">
                   <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
+                  Booking history
                 </a>
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="index.php">
                   <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
+                  Main Portal
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
